17c837c5ecd71abafdd362cc7fa32aa7a3f6b78e
Wells Johnston <wellsjohnston@gmail.com>
2016-05-11 22:43:55 -0400

    show spread in chart

--- main.py
+++ main.py
@@ -14,7 +14,17 @@ def graphRawFX():
     ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan=40, colspan=40)
     ax1.plot(date, bid)
     ax1.plot(date, ask)
+    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+
+    for label in ax1.xaxis.get_ticklabels():
+        label.set_rotation(45)
+
+    ax1_2 = ax1.twinx()
+    ax1_2.fill_between(date, 0, (ask-bid), facecolor='g', alpha=.3)
+
+    plt.subplots_adjust(bottom=.23)
+
     plt.grid(True)
     plt.show()
 
